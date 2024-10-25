ef4ef3214c0086a189ed8ba3a0d7cfd393790101
Piyush Dubey <piyush115dubey@gmail.com>
2024-10-25 17:26:33 +0530

    removed unused api

--- app/api/thread/[threadId]/route.ts
+++ app/api/thread/[threadId]/route.ts
@@ -1,10 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { OpenAIAssistant } from "@/lib/openai";
-import Thread from "@/models/Threads";
-import connectDB from "@/app/lib/db";
 import OpenAI from "openai";
 export const assistants = ["asst_X6Xp1GZxWu2eQrR8GKUnzQrn"];
-const API_KEY = process.env.OPENAI_API_KEY || "";
 
 const openai = new OpenAI();
 
@@ -17,7 +13,7 @@ export async function GET(
   }
 ) {
   try {
-    req.json()
+    req.json();
     const { threadId } = await params;
 
     const myThread = await openai.beta.threads.retrieve(threadId);
@@ -29,24 +25,6 @@ export async function GET(
     }
 
     return NextResponse.json({ thread: myThread }, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: error instanceof Error ? error.message : "An error occurred" },
-      { status: 500 }
-    );
-  }
-}
-export async function POST() {
-  try {
-    await connectDB();
-
-    const OpenAIAssistantInstance = new OpenAIAssistant(API_KEY);
-    const newThreadId = await OpenAIAssistantInstance.createThread();
-    const thread = new Thread({ threadId: newThreadId });
-    await thread.save();
-
-    return NextResponse.json({ thread }, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
